73e81282603e43401366460ace58112e3128f24c
bzirs <bzirs@outlook.com>
2023-02-01 19:45:04 +0800

    chore: 🔧 修改适配配置

--- craco.config.ts
+++ craco.config.ts
@@ -1,5 +1,12 @@
 // const path = require('path')
 import { resolve } from 'path'
+// import pxToViewport from 'postcss-px-to-viewport'
+const pxToViewport = require('postcss-px-to-viewport')
+
+const vw = pxToViewport({
+  // 视口宽度，一般就是 375（ 设计稿一般采用二倍稿，宽度为 375 ）
+  viewportWidth: 375
+})
 
 module.exports = {
   // webpack 配置
@@ -10,6 +17,12 @@ module.exports = {
       '@': resolve(__dirname, 'src'),
       // 约定：使用 @scss 表示 样式 文件所在路径
       '@scss': resolve(__dirname, 'src', 'assets', 'styles')
-    },
+    }
   },
+  // 这里补充style配置
+  style: {
+    postcss: {
+      plugins: [vw]
+    }
+  }
 }
