b6b5f58e99bb6c4f9ace238f73fc65618c20baff
bzirs <bzirs@outlook.com>
2023-02-01 21:13:59 +0800

    feat: ✨ 登录页

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,23 @@
-import { Button } from 'antd-mobile'
-
 import styles from './index.module.scss'
+import { Form, Input, NavBar } from 'antd-mobile'
+import { FormItem } from 'antd-mobile/es/components/form/form-item'
 const Login = () => {
-  return (
-    <div>
-      <Button block color='primary' size='large' className={styles.btn}>
-        登录
-      </Button>
+  const back = () => {}
 
-      <div className={styles.box1}></div>
+  return (
+    <div className={styles.root}>
+      {/* NavBar */}
+      <NavBar onBack={back}></NavBar>
+      {/* 主体 */}
+      <div className='login-form'>
+        <h2 className='title'>短信登录</h2>
+        {/* 表单内容 */}
+        <Form>
+          <FormItem className='login-item' name='mobile'>
+            <Input placeholder='请输入手机号'></Input>
+          </FormItem>
+        </Form>
+      </div>
     </div>
   )
 }
